8ec7b13456f015f65d5e61c01d2eef7c80e3d729
Tin Danh Nguyen <t.nguyen@ibm.com>
2021-09-06 21:57:52 -0400

    Add HelloWorld file.

--- scripts/run-graph.sh
+++ scripts/run-graph.sh
@@ -1,9 +1,17 @@
-export WDIR="/home/luheng/Working/pr-graph"
+export WDIR="/home/gridsan/tdn/pr-graph"
 export CLASSPATH="$WDIR/bin/:$WDIR/libs/optimization-2010.11.jar:$WDIR/libs/trove-2.0.2.jar:$WDIR/libs/args4j-2.0.10.jar"
 
 lang=$1
 lname=$2
 umap=$3
+
+echo $lang
+echo $lname
+echo $umap
+
+umapPath="$WDIR/data/univmap/$lang-$umap.map"
+echo $umapPath
+cat $umapPath
 
 java -cp $CLASSPATH -Xmx8000m programs.TestPosGraphBuilder  \
 -data-path "$WDIR/data/langs/$lang/$lname.train,$WDIR/data/langs/$lang/$lname.test" \
